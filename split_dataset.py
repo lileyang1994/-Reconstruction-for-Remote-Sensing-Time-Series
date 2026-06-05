e277dac4c5db75684dfcbafcc534185d0a254017
Leyang <49427014+lileyang1994@users.noreply.github.com>
2026-06-05 13:17:34 +0800

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-X = np.load(r"D:\Learn\database\MOD13Q1\Processed/X.npy")
-Mask = np.load(r"D:\Learn\database\MOD13Q1\Processed/Mask.npy")
-X_obs = np.load(r"D:\Learn\database\MOD13Q1\Processed/X_obs.npy")
+X = np.load(r".\Processed/X.npy")
+Mask = np.load(r".\Processed/Mask.npy")
+X_obs = np.load(r".\MOD13Q1\Processed/X_obs.npy")
 
 idx = np.arange(len(X))
 
@@ -19,9 +19,9 @@ train_idx, val_idx = train_test_split(
     random_state=42
 )
 
-np.save(r"D:\Learn\database\MOD13Q1\Processed/train_idx.npy", train_idx)
-np.save(r"D:\Learn\database\MOD13Q1\Processed/val_idx.npy", val_idx)
-np.save(r"D:\Learn\database\MOD13Q1\Processed/test_idx.npy", test_idx)
+np.save(r".\Processed/train_idx.npy", train_idx)
+np.save(r".\MOD13Q1\Processed/val_idx.npy", val_idx)
+np.save(r".\MOD13Q1\Processed/test_idx.npy", test_idx)
 
 print("Train:", len(train_idx))
 print("Val:", len(val_idx))
